0834634bb16b9e630ccf72e9efc80c1bf154a9f4
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-02-28 17:24:18 +0530

    Version before I gave up and started new

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
 # Configure application
-app = Flask(__name__)
+app = Flask(__name__) 
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-@app.route("/login")
+@app.route("/")
 def index():
     return render_template("login.html")

46801b961d2a00039cd6c69e507201f54a32f644
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-03-02 00:03:17 +0530

    added base.html file in templates without modifying app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('home')
+@app.route('/home')
 def index():
     return render_template("starter.html")
+
+@app.route('/market')
+def market():
+    return render_template('market.html')

74c929c6e4d458aff11a43f84cce2c6e222aef57
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-03-03 18:22:39 +0530

    progress

--- app.py
+++ app.py
@@ -5,16 +5,20 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def index():
-    return render_template("layout.html")
+    return render_template("home.html")
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
-@app.route('/fatwa')
-def fatwa():
-    return render_template('fatwa.html')
-
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/guide')
+def guide():
+    return render_template('guide.html')
+
+@app.route('/fatwa')
+def fatwa():
+    return render_template('fatwa.html')
